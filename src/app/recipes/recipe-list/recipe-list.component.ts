d6c3fc38614c81e8674a4f2b4241d68cb59ab81f
gpdelaney <gpdelaney@gmail.com>
2019-10-14 23:26:46 +0200

    Refactoring Recipes to use @Input and @Output and event emitters (to be improved and simplified in the future). Also minor styling for pictures.

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -10,8 +10,12 @@ export class RecipeListComponent implements OnInit {
   // tslint:disable-next-line:max-line-length
   recipes: Recipe[] = [new Recipe('Empanadas', 'Small pies filled with meat',
     'https://t1.rg.ltmcdn.com/es/images/5/8/4/img_empanadas_de_carne_cortada_a_cuchillo_7485_600.jpg')];
-
+  @Output() recipeComponent = new EventEmitter<Recipe>();
   constructor() {
+  }
+
+  onRecipeSelected(recipe: Recipe) {
+    this.recipeComponent.emit(recipe);
   }
 
   ngOnInit() {
