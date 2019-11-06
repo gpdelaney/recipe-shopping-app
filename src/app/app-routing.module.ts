f09b6b2a742698dbcdde1cfbf70d60282a39610d
gpdelaney <gpdelaney@gmail.com>
2019-11-06 19:53:11 +0100

    Removing old href's and adding a default routing to navigate to recipes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,11 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
 import {ShoppingComponent} from './shopping/shopping.component';
 import {RecipesComponent} from './recipes/recipes.component';
 
 
 const routes: Routes = [
+  {path: '', redirectTo: '/recipes', pathMatch: 'full'},
   {path: 'shopping', component: ShoppingComponent},
   {path: 'recipes', component: RecipesComponent}];
 
@@ -12,4 +13,5 @@ const routes: Routes = [
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
